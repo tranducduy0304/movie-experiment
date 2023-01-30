062185194c0897bec557c94336052607d0c981a9
tranducduy0304 <54491223+tranducduy0304@users.noreply.github.com>
2023-01-30 23:13:14 +0700

    Update file

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,34 @@
-import './globals.css'
+import "./globals.css";
+import { Montserrat } from "@next/font/google";
+
+const montserrat = Montserrat({
+    weight: ["400", "700"],
+    subsets: ["latin"],
+    variable: "--font-montserrat",
+});
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      {/*
+    return (
+        <html lang="en">
+            {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+            <head />
+            <body className={`${montserrat.className}`}>
+                {children}
+                <nav>
+                    <h1>logo</h1>
+                    <ul>
+                        <li>
+                            <a href="#">about</a>
+                        </li>
+                        <li>
+                            <a href="#">signup</a>
+                        </li>
+                    </ul>
+                </nav>
+            </body>
+        </html>
+    );
 }
